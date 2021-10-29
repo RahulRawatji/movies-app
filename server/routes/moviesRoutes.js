90d478255dac9f5bbb8ea8af5666d0e6fac5fcf4
Rahul Rawat <rahul.rawat3@byjus.com>
2021-10-29 22:10:20 +0530

    Fixed Search Funtion

--- server/routes/moviesRoutes.js
+++ server/routes/moviesRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
-const { getAllMovies , getMovie, addMovie } = require('../controllers/movieController');
+const { getAllMovies , getMovie, addMovie , getTopRated } = require('../controllers/movieController');
 
 const router = express.Router();
 
 router.get("/",getAllMovies);
 router.get("/:movieId",getMovie);
+router.get("/topRated",getTopRated);
 router.post("/",addMovie);
 
 
